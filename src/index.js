82f9246b83b76e7166048a8d79d622895972c4bb
unknown <varachkin@gmail.com>
2021-03-15 21:03:11 +0300

    init: add index.js. 63 test done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array){
+        if(array.length > 0){
+            let minValue = 0;
+            for(let i = 0; i < array.length; i++){
+                if(array[i] < minValue){
+                    minValue = array[i];
+                }
+            }
+            return minValue;
+        }else{
+            return 0;
+        }
+    }else{
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array){
+        if(array.length > 0) {
+            let maxValue = 0;
+            for(let i = 0; i < array.length; i++){
+                if(array[i] > maxValue){
+                    maxValue = array[i];
+                }
+            }
+            return maxValue;
+        }else{
+            return 0;
+        }
+    }else{
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array){
+        if (array.length > 0){
+            let sum = 0;
+            let n = 0;
+            if(array.length > 0){
+                for(let i = 0; i < array.length; i++){
+                    sum += array[i];
+                    n++;
+                }
+            }
+            return sum / n;
+        }else{
+            return 0;
+        }
+    }else{
+        return 0;
+    }
 }
